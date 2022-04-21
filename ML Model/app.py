fd1d0bf9ec5e08be7df1ab926bbe46e12b2dcf1a
saradindurana <saradindurana@gmail.com>
2022-04-21 08:04:32 +0530

    changed as per discussed yesterday

--- ML Model/app.py
+++ ML Model/app.py
@@ -7,7 +7,7 @@ import pickle
 from flask_cors import CORS
 import numpy as np
 from pymongo import MongoClient
- 
+import datetime,time
 
 #connect
 try:
@@ -18,7 +18,7 @@ except:
 
 model=[]
 for i in range (1,11):
-   file='C://xampp//htdocs//vehicle//'+'JunModel'+str(i)+'.pkl'
+   file='C://Users//Admin//Desktop//EV-dynamic-pricing//ML Model'+'JunModel'+str(i)+'.pkl'
    m1=pickle.load(open(file,'rb'))
    model.append(m1)
 
@@ -30,100 +30,99 @@ CORS(app)
 def price():
     while True:
         getprice()
-        time.sleep(10)
+        time.sleep(3600)
     
     
 if __name__=="__main__":
     app.run(debug=True)
 
 def getprice():
+    predictedLambda=getPredictedLambda()
+    predictedSum=0
+    allprices=[]
+    for predicted in predictedLambda:
+        predictedSum = predictedSum + predicted
     for i in range(1,11):
         previousPrice=getPreviousPrice(i)
-        optimalLambda=getLambdaOptimal(i)
-        predictedLambda=getPredictedLambda(i)
-        difference=predictedLambda-optimalLambda
+        optimalLambda=getLambdaOptimal(predictedSum,i)
+        difference=predictedLambda[i] - optimalLambda
         nextPrice = previousPrice + getGamma()*difference
-    #save to database
-        jun='jun'+str(i)
-        dt = datetime.now()
-        id= jun+"_"+str(dt.year)+str(dt.month)+str(dt.day)+str(dt.hour)
-        mydb = client.EV
-        mytab = mydb[jun]
+        allprices.append(nextPrice)
+    #save to database this part needs to be change
+        # jun='jun'+str(i)
+        # dt = datetime.now()
+        # id= jun+"_"+str(dt.year)+str(dt.month)+str(dt.day)+str(dt.hour)
+        # mydb = client.EV
+        # mytab = mydb[jun]
         
-        rec = mytab.insert_one({
-        "id" : id,
-        "Arrival_rate" : 0,
-        "Service_rate" : 0,
-        "Previous_price" :0,
-        "cur_price" : nextPrice,
-        "time" : dt
-        })
-    
-        #return jsonify(nextPrice)
-        #fuck previous price
-        #fuck Arrival rate
+        # rec = mytab.insert_one({
+        # "id" : id,
+        # "Arrival_rate" : 0,
+        # "Service_rate" : 0,
+        # "Previous_price" :0,
+        # "cur_price" : nextPrice,
+        # "time" : dt
+        # })
+    return jsonify(allprices)
+      
+def getPredictedLambda():
+    #arr = np.array([[11,6,0,1,2015,11,2]])
+    predicted=[]
+    for i in range(1,11):
+        dt = datetime.now()
+        arr=np.array([[dt.day, dt.weekday(), dt.hour, dt.month, dt.year, datetime.now().timetuple().tm_yday, datetime.utcnow().isocalendar()[1]]])
+        
+        predict = model[i].predict(arr)
+        predicted.append(predict)
+        
+    return predicted 
 
-def getLambdaOptimal(i):
-    lambSum = getSumoflambda();
-    serviceRate=getServicerate();
-    sum=0
+def getLambdaOptimal(sum,i):
+    lambSum = sum
+    serviceRate=getServicerate()
+    uppersum=0
     lowersum=0
     for rate in serviceRate:
         x = math.sqrt(serviceRate[i]*rate)-rate
-        sum = sum+x
+        uppersum = uppersum+x
         lower=rate//serviceRate[i]
         lower=math.sqrt(lower)
         lowersum=lowersum+lower
-    upper_side=lambSum+sum
+    upper_side=lambSum+uppersum
     lambdaOptimal=upper_side//lowersum
     return lambdaOptimal
 
-def getPredictedLambda(i):
-    #arr = np.array([[11,6,0,1,2015,11,2]])
-    dt = datetime.now()
-    arr=np.array([[dt.day, dt.weekday(), dt.hour, dt.month, dt.year, datetime.now().timetuple().tm_yday, datetime.utcnow().isocalendar()[1]]])
-    
-    predicted = model[i].predict(arr)
-    predicted = float(predicted)
-    return predicted 
 
-
+# check this part again
 def getPreviousPrice(i):
     a = "jun" + str(i)
     mydb = client.EV
     #mytab = mydb.test
     new=dict(mydb.a.find().limit(1).sort([('$natural', -1)]).next())
     prevPrice=new['Previous_price']
-    #print(new['Previous_price'])
-    #prices = [ 10,11,12,13,14,15]
-    #prevPrice = random.choice(prices)
     return prevPrice
 
 def getGamma():
     gamma = 0.5
     return gamma
 
-def getSumoflambda():
-    sum=0
-    for i in range(1,11):
-        jun='jun'+str(i)
-        mydb = client.EV
-        mytab = mydb[jun]
-        new=dict(mydb[jun].find().limit(1).sort([('$natural', -1)]).next())
-
-        sum=sum+new['Arrival_rate']
-
-    return sum
 
 def getServicerate():
     li=[]
     for i in range(1,11):
-        jun='jun'+str(i)
-        mydb = client.EV
-        mytab = mydb[jun]
-        new=dict(mydb[jun].find().limit(1).sort([('$natural', -1)]).next())
-        print(new['Service_rate'])
-        li.append(new['Service_rate'])
+        ports=getNumberofPorts()
+        chargetime=getchargeTime()
+        serviceRate=(60/chargetime)*ports
+        li.append(serviceRate)
 
     return li
-   
+
+def getNumberofPorts():
+    #   get from database
+    NumberofPorts=5 #change this
+    return NumberofPorts
+
+def getchargeTime():
+    #   get from database
+    chargeTime=20 #change this
+    return chargeTime
