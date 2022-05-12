339a42ca2d5f437ee261324e34261fd7d727f414
saradindurana <saradindurana@gmail.com>
2022-05-12 17:20:08 +0530

    charttt

--- User Interface/Admin/pricegraph.php
+++ User Interface/Admin/pricegraph.php
@@ -1,6 +1,6 @@
 <?php require 'vendor/autoload.php';
 $server = "mongodb://localhost:27017";
-$dt = $_POST['data'];
+// $dt = $_POST['data'];
 
 try{// Connecting to server
 $c = new MongoDB\Client($server);}
@@ -9,13 +9,15 @@ print $connectionException;
 exit;}
 $db = $c->ElectricVehicle;
 $data=array();
+for($i=0;$i<10;$i++){
 
-    $collection_name='jun'.$dt;
+    $collection_name='jun'.$i;
     $collection = $db->$collection_name;
     $result=$collection->find([],['sort' => ['$natural'=>-1],'limit'=>1]);
     foreach($result as $r){
         $data[]=$r;
     }
+}
 
 echo json_encode($data);
 ?>
